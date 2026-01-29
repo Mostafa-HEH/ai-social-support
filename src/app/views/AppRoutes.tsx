001940fbff655d46dc2a87194c70e0630025b345
Mostafa Neamatalla <34992292+Mostafa-HEH@users.noreply.github.com>
2026-01-29 21:02:26 +0400

    fix: fix direction issue

--- src/app/views/AppRoutes.tsx
+++ src/app/views/AppRoutes.tsx
@@ -8,12 +8,18 @@ import getMuiTheme from "../../shared/theme/mui";
 import { CacheProvider } from "@emotion/react";
 import { ltrCache, rtlCache } from "../../shared/theme";
 import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
 
 const AppRoutes = () => {
   const { i18n } = useTranslation();
 
   const isRTL = i18n.language === "ar";
   const theme = getMuiTheme(isRTL ? "rtl" : "ltr");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("dir", isRTL ? "rtl" : "ltr");
+    document.documentElement.setAttribute("lang", i18n.language);
+  }, [isRTL, i18n.language]);
 
   return (
     <BrowserRouter>
